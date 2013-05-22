7da6fe855982fbc79d4fedc0cc46b23e38e58b8a
Nigel Stanger <nigel.stanger@otago.ac.nz>
2013-05-22 17:36:13 +1200

    Rewrote to use onload event listeners (closes #6).

--- plugin/position-images/position-images.js
+++ plugin/position-images/position-images.js
@@ -26,7 +26,11 @@
  * agnostic ;) to make the image scale to fit its container as best as possible.
  *
  */
-var positionImages = function()
+
+// Needs to be global so other plugins can detect whether this has been loaded.
+var positionImage;
+
+(function()
 {
 	/*
 		A cross-browser weirdness: in Firefox, max-height (or max-width) for the
@@ -59,6 +63,97 @@ var positionImages = function()
 	}
 	
 	
+	positionImage = function( img, containerDIV, parentSection, listener )
+	{
+		// The container needs to be at least position: relative, and the image position: absolute.
+		containerDIV.style.position = 'relative';
+		img.style.position = 'absolute';
+	
+		var	originalHeight = img.height,
+			originalWidth = img.width,
+			targetHeight = originalHeight,
+			targetWidth = originalWidth;
+	
+		/*
+			The dimensions of the containing DIV parent may be invalid if the
+			section is currently not visible (i.e., display: none). We therefore
+			need to briefly ensure that the section element is visible (i.e.,
+			display: block) then set it back to it's original state.
+		*/
+		var originalVisibility = parentSection.style.display;
+		parentSection.style.display = "block";
+		
+		var	parentHeight = containerDIV.clientHeight,
+			parentWidth = containerDIV.clientWidth;
+		
+		parentSection.style.display = originalVisibility;
+		
+		/*
+			We need to adjust for any margins that might be specified, otherwise
+			the image size and positioning will off from what we would expect.
+		*/
+		var	imgMarginTop = parseCSSValue( window.getComputedStyle( img ).marginTop, parentHeight ),
+			imgMarginBottom = parseCSSValue( window.getComputedStyle( img ).marginBottom, parentHeight ),
+			imgMarginRight = parseCSSValue( window.getComputedStyle( img ).marginRight, parentWidth ),
+			imgMarginLeft = parseCSSValue( window.getComputedStyle( img ).marginLeft, parentWidth );
+		
+		/*
+			Hide the image before we start fiddling with it. This reduces the
+			number of visible transitions that can occur, and may be faster?
+		*/
+		originalVisibility = img.style.display;
+		img.style.display = "none";
+		
+		// Maximise the dimensions of the image if appropriate.
+		if ( img.classList.contains( 'maximize' ) || img.classList.contains( 'maximise' ) ) // dialect agnostic :)
+		{
+			// Figure out the orientation of the image so that we can maximise the appropriate dimension.
+			if ( ( originalWidth / parentWidth ) > ( originalHeight / parentHeight ) )
+			{
+				targetWidth = parseCSSValue( window.getComputedStyle( img ).maxWidth, parentWidth ) - imgMarginRight - imgMarginLeft;
+				targetHeight = ( targetWidth / originalWidth * originalHeight ) >> 0;
+			}
+			else
+			{
+				targetHeight = parseCSSValue( window.getComputedStyle( img ).maxHeight, parentHeight ) - imgMarginBottom - imgMarginTop;
+				targetWidth = ( targetHeight / originalHeight * originalWidth ) >> 0;
+			}
+			
+			img.style.height = targetHeight + "px";
+			img.style.width = targetWidth + "px";
+		}
+		
+		/*
+			Apply any alignment classes. In cases of conflict (e.g.,
+			class="top bottom"), the last class specified wins.
+		*/
+		var alignClasses = img.classList;
+		for ( var a = 0, aLen = alignClasses.length; a < aLen; a++ )
+		{
+			var thisClass = alignClasses[a];
+			
+			if ( thisClass === "top" ) img.style.top = "0px";
+			if ( thisClass === "middle" ) img.style.top = ( ( parentHeight - targetHeight -imgMarginTop - imgMarginBottom ) >> 1 ) + "px";
+			// We could just set bottom, but using the same dimension consistently ensures that later classes override earlier ones.
+			if ( thisClass === "bottom" ) img.style.top = ( parentHeight - targetHeight - imgMarginBottom ) + "px";
+			if ( thisClass === "left" ) img.style.left = "0px";
+			if ( ( thisClass === "center" ) || ( thisClass === "centre" ) ) img.style.left = ( ( parentWidth - targetWidth - imgMarginLeft - imgMarginRight ) >> 1 ) + "px";
+			if ( thisClass === "right" ) img.style.left = ( parentWidth - targetWidth - imgMarginRight ) + "px";
+		}
+		
+		img.style.display = originalVisibility;
+		
+// 		console.log( "position img " + img.id + ": " + img.width + " × " + img.height );
+	}
+	
+	
+	/*
+		We need to make sure that we only run the positioning code once the images
+		are actually loaded, otherwise the dimensions, etc., will all be wrong. The
+		solution is to iterate through each image, and if it isn't loaded yet, add
+		an onload event listener. We don't need to worry about caching vs. onload,
+		because all we care about is that the image is loaded.
+	*/
 	var	imgContainers = document.querySelectorAll( 'div.img-container' );
 	
 	for ( var c = 0, cLen = imgContainers.length; c < cLen; c++ )
@@ -67,99 +162,32 @@ var positionImages = function()
 			parentSection = containerDIV,
 			alignedImages = containerDIV.querySelectorAll( 'img.align' );
 		
+		// Find the parent section element. No nice, easy way to do this.
 		while ( parentSection && parentSection.tagName !== 'SECTION' ) parentSection = parentSection.parentNode;
 		if ( !parentSection )
 		{
-			// throw an exception?
+			throw "presentation contains a DIV with no parent SECTION";
 		}
 		
 		for ( var i = 0, iLen = alignedImages.length; i < iLen; i++ )
 		{
 			var	img = alignedImages[i];
 			
-			// The container need to be at least position: relative, and the image position: absolute.
-			containerDIV.style.position = 'relative';
-			img.style.position = 'absolute';
-	
-			var	originalHeight = img.height,
-				originalWidth = img.width,
-				targetHeight = originalHeight,
-				targetWidth = originalWidth;
-	
 			/*
-				The dimensions of the containing DIV parent may be invalid if the
-				section is currently not visible (i.e., display: none). We therefore
-				need to briefly ensure that the section element is visible (i.e.,
-				display: block) then set it back to it's original state.
+				If the image has finished loading, go ahead and position it, otherwise
+				install a listener to run when it eventually does load.
 			*/
-			var originalVisibility = parentSection.style.display;
-			parentSection.style.display = "block";
-			
-			var	parentHeight = containerDIV.clientHeight,
-				parentWidth = containerDIV.clientWidth;
-			
-			parentSection.style.display = originalVisibility;
-			
-			/*
-				We need to adjust for any margins that might be specified, otherwise
-				the image size and positioning will off from what we would expect.
-			*/
-			var	imgMarginTop = parseCSSValue( window.getComputedStyle( img ).marginTop, parentHeight ),
-				imgMarginBottom = parseCSSValue( window.getComputedStyle( img ).marginBottom, parentHeight ),
-				imgMarginRight = parseCSSValue( window.getComputedStyle( img ).marginRight, parentWidth ),
-				imgMarginLeft = parseCSSValue( window.getComputedStyle( img ).marginLeft, parentWidth );
-			
-			/*
-				Hide the image before we start fiddling with it. This reduces the
-				number of visible transitions that can occur, and may be faster?
-			*/
-			originalVisibility = img.style.display;
-			img.style.display = "none";
-			
-			// Maximise the dimensions of the image if appropriate.
-			if ( img.classList.contains( 'maximize' ) || img.classList.contains( 'maximise' ) ) // dialect agnostic :)
+			if ( img.complete )
 			{
-				// Figure out the orientation of the image so that we can maximise the appropriate dimension.
-				if ( ( originalWidth / parentWidth ) > ( originalHeight / parentHeight ) )
-				{
-					targetWidth = parseCSSValue( window.getComputedStyle( img ).maxWidth, parentWidth ) - imgMarginRight - imgMarginLeft;
-					targetHeight = ( targetWidth / originalWidth * originalHeight ) >> 0;
-				}
-				else
-				{
-					targetHeight = parseCSSValue( window.getComputedStyle( img ).maxHeight, parentHeight ) - imgMarginBottom - imgMarginTop;
-					targetWidth = ( targetHeight / originalHeight * originalWidth ) >> 0;
-				}
-				
-				img.height = targetHeight;
-				img.width = targetWidth;
+				positionImage( img, containerDIV, parentSection );
+			}
+			else
+			{
+				var listener = ( function() { positionImage( this, containerDIV, parentSection, listener ); } );
+				img.addEventListener( 'load', listener, false );
+// 				console.log( ">>> added event handler to img " + img.id );
 			}
 			
-			/*
-				Apply any alignment classes. In cases of conflict (e.g.,
-				class="top bottom"), the last class specified wins.
-			*/
-			var alignClasses = img.classList;
-			for ( var a = 0, aLen = alignClasses.length; a < aLen; a++ )
-			{
-				var thisClass = alignClasses[a];
-				
-				if ( thisClass === "top" ) img.style.top = "0px";
-				if ( thisClass === "middle" ) img.style.top = ( ( parentHeight - targetHeight -imgMarginTop - imgMarginBottom ) >> 1 ) + "px";
-				// We could just set bottom, but using the same dimension consistently ensures that later classes override earlier ones.
-				if ( thisClass === "bottom" ) img.style.top = ( parentHeight - targetHeight - imgMarginBottom ) + "px";
-				if ( thisClass === "left" ) img.style.left = "0px";
-				if ( ( thisClass === "center" ) || ( thisClass === "centre" ) ) img.style.left = ( ( parentWidth - targetWidth - imgMarginLeft - imgMarginRight ) >> 1 ) + "px";
-				if ( thisClass === "right" ) img.style.left = ( parentWidth - targetWidth - imgMarginRight ) + "px";
-			}
-			
-			img.style.display = originalVisibility;
 		}
 	}
-}
-
-/*
-	Ensure that we only run once the document is ready, otherwise images may not
-	have completed loaded and have invalid dimensions.
-*/
-head.ready( document, positionImages() )
+})();
